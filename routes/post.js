7afb2c203cbb75613c4bd60b0ec82515848d2cd3
ronaldmark.antonio <ronaldmark.antonio@gmail.com>
2025-10-21 02:09:24 +0800

    Update posts/getPost/postId

--- routes/post.js
+++ routes/post.js
@@ -9,7 +9,7 @@ router.post("/addPost", verify, postController.addPost);
 router.patch("/updatePost/:postId", verify, postController.updatePost);
 router.delete("/deletePost/:postId", verify, postController.deletePost);
 router.get("/getPosts", postController.getAllPosts);
-router.get("/getPosts/:postId", postController.getMovieById);
+router.get("/getPost/:postId", postController.getMovieById);
 router.patch("/addComment/:postId", postController.addPostComment);
 router.get("/getComment/:postId", postController.getPostComment);
 router.delete("/deleteComment/:commentId", verify, verifyAdmin, postController.deleteComment);
